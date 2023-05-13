d4945f7dd51fa02e4222a5fd9645168f14484d8c
Arnaud <contact@awb-dsgn.com>
2023-05-13 15:25:32 +0200

    up date comment

--- translations/index.php
+++ translations/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* 2007-2018 PrestaShop
+* 2007-2023 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -19,7 +19,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author Arnaud Drieux <contact@awb-dsgn.com>
-*  @copyright  2007-2018 awb-dsgn.com
+*  @copyright  2007-2023 awb-dsgn.com
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
