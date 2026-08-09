dac477deb17c591c3ac5fd2b310088736bfef268
ArnaudDx <adx.mlight@gmail.com>
2026-08-09 12:09:07 +0200

    Simplification des en-têtes de licence

--- storeggmap.php
+++ storeggmap.php
@@ -1,29 +1,11 @@
 <?php
-/*
-* 2007-2023 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author Arnaud Drieux <contact@awb-dsgn.com>
-*  @copyright  2007-2024 awb-dsgn.com
-
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
+/**
+ * storeggmap - Show your stores on a Google Map
+ *
+ * @author    Arnaud Drieux <contact@awb-dsgn.com>
+ * @copyright 2026 awb-dsgn.com
+ * @license   https://opensource.org/licenses/AFL-3.0 AFL-3.0
+ */
 
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -66,7 +48,7 @@ class Storeggmap extends Module implements WidgetInterface
         $this->displayName = $this->l('Show your stores on a google map');
         $this->description = $this->l('Add Google map on the store page');
 
-        $this->ps_versions_compliancy = array('min' => '1.7.0.0', 'max' => '9.99.99');
+        $this->ps_versions_compliancy = array('min' => '1.7.0.0', 'max' => '8.99.99');
 
         $this->templateFile = 'module:storeggmap/views/templates/hook/storeggmap.tpl';
         $this->templateDetailFile = 'module:storeggmap/views/templates/front/storeggmap_detail.tpl';
--- views/templates/hook/storeggmap.tpl
+++ views/templates/hook/storeggmap.tpl
@@ -1,27 +1,10 @@
 {*
-* 2007-2023 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author Arnaud Drieux <contact@awb-dsgn.com>
-*  @copyright  2007-2023 awb-dsgn.com
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*}
+ * storeggmap - Show your stores on a Google Map
+ *
+ * @author    Arnaud Drieux <contact@awb-dsgn.com>
+ * @copyright 2026 awb-dsgn.com
+ * @license   https://opensource.org/licenses/AFL-3.0 AFL-3.0
+ *}
 
 <section id="map-style">
     {if $apiKey}
